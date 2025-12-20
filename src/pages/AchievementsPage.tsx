4ebb2483ef640a61387a8ba4f7c51877ce40b31e
KGFCH2 <babinbid05@gmail.com>
2025-12-20 21:28:18 +0530

    Particle Background Removed from Achievements page

--- src/pages/AchievementsPage.tsx
+++ src/pages/AchievementsPage.tsx
@@ -1,10 +1,5 @@
-import { ThemeProvider } from "next-themes";
 import { useEffect } from "react";
-import ParticlesBackground from "@/components/ParticlesBackground";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import BackToTop from "@/components/BackToTop";
-import SkipToContent from "@/components/SkipToContent";
+import PageLayout from "@/components/PageLayout";
 import Achievements from "@/components/Achievements";
 
 const AchievementsPage = () => {
@@ -13,18 +8,9 @@ const AchievementsPage = () => {
     }, []);
 
     return (
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-            <div className="relative min-h-screen">
-                <SkipToContent />
-                <ParticlesBackground />
-                <Header />
-                <main id="main-content" className="pt-16">
-                    <Achievements />
-                </main>
-                <Footer />
-                <BackToTop />
-            </div>
-        </ThemeProvider>
+        <PageLayout>
+            <Achievements />
+        </PageLayout>
     );
 };
 
